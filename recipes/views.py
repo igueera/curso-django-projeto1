a9cf2ec14d802340562a4b695ad9519ec4e0c9bf
Igor Gabriel <igoramsouza@gmail.com>
2025-10-19 18:00:26 -0300

    adicionado recipe-view template

--- recipes/views.py
+++ recipes/views.py
@@ -6,3 +6,8 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Igor Gabriel'
     })
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'name': 'Igor Gabriel'
+    })
